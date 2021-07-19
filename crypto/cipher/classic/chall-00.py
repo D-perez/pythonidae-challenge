cafafe8bd117fa24560a6c81db26f2afee5f70bf
D-perez <d27.sec@outlook.com>
2021-07-18 21:15:32 -0700

    Update chall-00.py

--- crypto/cipher/classic/chall-00.py
+++ crypto/cipher/classic/chall-00.py
@@ -8,7 +8,7 @@ alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
 # Solution 1
 
-# 
+# Uses a for loop and modulo to shift the index of the letter in the alphabet by 13.
 
 rotated_text = ''
 
@@ -20,7 +20,9 @@ print( 'solution 1: ' + rotated_text)
 
 
 # Solution 2
-# 
+
+# Uses a table to translate the a-z alphabet to n-za-m 
+
 
 table = str.maketrans(alphabet, (alphabet[13:]+alphabet[0:13]))
 print( 'solution 2: ' + clear_text.translate(table))
