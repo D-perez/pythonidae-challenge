238fffa0a9884a41d660ed5e5628fbbd2d1f9649
D-perez <d27.sec@outlook.com>
2021-07-19 19:50:42 -0700

    Mon Jul 19 19:50:42 PDT 2021

---
+++ crypto/cipher/classic/chal-02.py
@@ -0,0 +1,37 @@
+import sys
+
+# Solution
+
+a = int(sys.argv[1])
+b = int(sys.argv[2])
+input_string = sys.argv[3]
+
+
+# encrypt
+
+# print(a,b,clear_text)
+
+def encrypt():
+    encrypted_text = ''
+    for letter in input_string:
+        if letter == ' ':
+            encrypted_text += ' '
+            continue
+        p = ord(letter) - 97 # p stands for position
+        encrypted_text += chr((a*p+b)% 26 + 97)
+    print(input_string,'\n',encrypted_text)
+# decrypt
+
+
+
+def decrypt():
+    decrypted_text = ''
+    for letter in input_string:
+        if letter == ' ':
+            decrypted_text += ' '
+            continue
+        p = ord(letter) - 97 # p stands for position
+        decrypted_text += chr(((a**-1) * (p - b) % 26) + 97)
+    print(decrypted_text)
+
+decrypt()
