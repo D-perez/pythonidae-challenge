7e86f8f5e12ea039f105423f8dd50d316ea8a83d
D-perez <d27.sec@outlook.com>
2021-07-22 16:55:43 -0700

    Thu Jul 22 16:55:43 PDT 2021

---
+++ crypto/cipher/classic/chal-04.py
@@ -0,0 +1,63 @@
+'''
+
+    A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+    ---------------------------------------------------
+A   A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+B   B C D E F G H I J K L M N O P Q R S T U V W X Y Z A
+C   C D E F G H I J K L M N O P Q R S T U V W X Y Z A B
+D   D E F G H I J K L M N O P Q R S T U V W X Y Z A B C
+E   E F G H I J K L M N O P Q R S T U V W X Y Z A B C D
+F   F G H I J K L M N O P Q R S T U V W X Y Z A B C D E
+G   G H I J K L M N O P Q R S T U V W X Y Z A B C D E F
+H   H I J K L M N O P Q R S T U V W X Y Z A B C D E F G
+I   I J K L M N O P Q R S T U V W X Y Z A B C D E F G H
+J   J K L M N O P Q R S T U V W X Y Z A B C D E F G H I
+K   K L M N O P Q R S T U V W X Y Z A B C D E F G H I J
+L   L M N O P Q R S T U V W X Y Z A B C D E F G H I J K
+M   M N O P Q R S T U V W X Y Z A B C D E F G H I J K L
+N   N O P Q R S T U V W X Y Z A B C D E F G H I J K L M
+O   O P Q R S T U V W X Y Z A B C D E F G H I J K L M N
+P   P Q R S T U V W X Y Z A B C D E F G H I J K L M N O
+Q   Q R S T U V W X Y Z A B C D E F G H I J K L M N O P
+R   R S T U V W X Y Z A B C D E F G H I J K L M N O P Q
+S   S T U V W X Y Z A B C D E F G H I J K L M N O P Q R
+T   T U V W X Y Z A B C D E F G H I J K L M N O P Q R S
+U   U V W X Y Z A B C D E F G H I J K L M N O P Q R S T
+V   V W X Y Z A B C D E F G H I J K L M N O P Q R S T U
+W   W X Y Z A B C D E F G H I J K L M N O P Q R S T U V
+X   X Y Z A B C D E F G H I J K L M N O P Q R S T U V W
+Y   Y Z A B C D E F G H I J K L M N O P Q R S T U V W X
+Z   Z A B C D E F G H I J K L M N O P Q R S T U V W X Y
+
+'''
+
+import sys
+
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+grid = [] # making the grid up there ^^^
+
+key =  sys.argv[1]
+text = sys.argv[2]
+
+
+if len(key) != len(text): # this makes sure the key repeats until it is the same length as the text
+    for i in range(len(text) - len(key)):
+        key += key[i]
+
+print(key)
+
+for i in range(len(alphabet)): # creates the grid
+    row = []
+    for letter in alphabet[i:]+alphabet[:i]: # shifts the characters each row
+        row.append(letter)
+    grid.append(row)
+
+
+
+
+e = ''
+
+for i in range(len(text)):
+    e += grid[alphabet.index(key[i])][alphabet.index(text[i])]
+
+print(e)
