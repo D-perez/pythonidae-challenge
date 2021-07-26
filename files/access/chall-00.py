b6a776fbe3a29a0f8f06b097316556391304178d
D-perez <d27.sec@outlook.com>
2021-07-25 20:13:54 -0700

    Sun Jul 25 20:13:54 PDT 2021

--- files/access/chall-00.py
+++ files/access/chall-00.py
@@ -1,9 +1,24 @@
 import os
 import sys
 
-your_path = sys.argv[1]
-files = os.listdir(your_path)
-keyword = 'your_keyword'
+
+your_path = sys.argv[1] if len(sys.argv) > 1 else '.'
+files = os.scandir(your_path)
 
 
-print(files)
+
+
+for file in files:
+    name = file.name
+    if len(name) > 17:
+        name = name[:17] + '...'
+    while len(name) < 20:
+        name += ' '
+    if file.is_dir():
+        name += ' directory' 
+    else:
+        name += ' file' 
+    print(name)
+
+
+
